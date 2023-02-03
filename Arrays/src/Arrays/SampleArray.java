477ffad3032236b9012e0600d1296c4fd4288567
Vaishnavi Matam <mvaishnavi2000@gmail.com>
2023-02-03 16:01:23 +0530

    Array Commit

---
+++ Arrays/src/Arrays/SampleArray.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SampleArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {1,2,3,4,5};
+		Arrays.sort(arr);
+		for(int ele: arr) {
+			System.out.print(ele+" ");
+		}
+		
+
+	}
+
+}
